2b02e73fcba9c5d50b219e94981579a9ab03e500
mikekauffman <mike.a.kauffman@gmail.com>
2014-05-14 09:58:21 -0600

    User can input a string and is returned a string that is centered on its longest line

---
+++ spec/text_centering_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require_relative '../lib/text_centering'
+
+describe CenterText do
+
+  it "takes a string and returns a string with the text centered against the longest line" do
+    input = <<-INPUT
+this is a short line
+this is a really long line that has a lot of characters
+and this is
+short
+    INPUT
+    text_input = CenterText.new(input)
+    expected = <<-EXPECTED
+                 this is a short line
+this is a really long line that has a lot of characters
+                      and this is
+                         short
+    EXPECTED
+    expect(text_input.centered).to eq(expected)
+  end
+
+end
